9ad8788f86cfb9cb0c230707392c0b9efe87db92
wagner <wagncarv@outlook.com>
2023-07-23 18:40:15 -0300

    Todo list component, conditional formatting

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Interface
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent {
-
+export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [
+    // { task: 'Estudar', checked: false },
+    // { task: 'Comer', checked: false },
+    // { task: 'Dormir', checked: true },
+  ];
+  constructor() {}
+  ngOnInit(): void {}
 }
